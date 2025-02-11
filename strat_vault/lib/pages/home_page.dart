12473fd4dd68749ac47787eebb85c3759b4904b8
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-11 20:37:53 +0200

    UI improvements: Intro & Home pages

--- strat_vault/lib/pages/home_page.dart
+++ strat_vault/lib/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:strat_vault/components/map_tile.dart';
 import 'package:strat_vault/models/map.dart';
+import 'package:strat_vault/pages/map_page.dart';
+import 'package:strat_vault/provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   
@@ -14,32 +16,78 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 
-  List maps = [
-    CSMap(name: 'Mirage', imagePath: 'lib/images/test_map.png'),
-    CSMap(name: 'Inferno', imagePath: 'lib/images/download.jpeg'),
-    CSMap(name: 'Dust 2', imagePath: 'lib/images/test_map.png'),
-    CSMap(name: 'Nuke', imagePath: 'lib/images/test_map.png'),
-    CSMap(name: 'Overpass', imagePath: 'lib/images/test_map.png')
-  ];
+  List<CSMap> maps = MapsProvider().getMaps;
+
+  void navigateToMapPage(CSMap map) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => MapPage(map: map)));
+  }
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 151, 228, 151),
+      key: scaffoldKey,
+      backgroundColor: const Color.fromARGB(255, 109, 120, 109),
+      drawer: Drawer(
+        
+        backgroundColor: const Color.fromARGB(227, 49, 55, 49),
+        child:  Column(
+          children: [
+            const SizedBox(height: 50),
+
+            ListTile(
+              title: const Text('S E T T I N G S', style: TextStyle(color: Colors.white), ),
+              leading: const Icon(Icons.settings, color: Colors.white),
+              enableFeedback: false,
+              onTap: () {
+                // go on settings page
+              },
+            ),
+
+            ListTile(
+              title: const Text('A B O U T', style: TextStyle(color: Colors.white), ),
+              leading: const Icon(Icons.info, color: Colors.white),
+              enableFeedback: false,
+              onTap: () {
+                // go on about page
+              },
+            ),
+            
+            ListTile(
+              title: const Text('C O N T A C T S', style: TextStyle(color: Colors.white), ),
+              leading: const Icon(Icons.contact_mail, color: Colors.white),
+              enableFeedback: false,
+              onTap: () {
+                // go on contacts page
+              },
+            ),
+          
+            const SizedBox(height: 200),
+
+            // at the end of the drawer should be the name or the app logo 
+            // and the version of the app
+
+          ],
+        ),
+        
+      ),
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         leading: IconButton(
-          icon: const Icon(Icons.menu, color: Colors.green),
-          onPressed: () {},
+          style: const ButtonStyle(
+            enableFeedback: false,
+          ),
+          icon: Image.asset('lib/images/menu.png', color: const Color.fromARGB(255, 19, 15, 15), width: 30, height: 30,),
+          onPressed: () {
+            scaffoldKey.currentState!.openDrawer();
+          },
         ),
         elevation: 0,
-        actions:[IconButton(
-          icon: const Icon(Icons.search, color: Colors.green),
-          onPressed: () {},
-        ),],
-        title: Text('MAP POOL', style: GoogleFonts.playfairDisplay(
-          color: const Color.fromARGB(255, 54, 20, 20),
+        title: const Text('MAP POOL', style: TextStyle(
+          color:  Color.fromARGB(255, 54, 20, 20),
           fontSize: 25,
+          fontFamily: 'Camcode',
         ),),
         centerTitle: true,
 
@@ -47,15 +95,24 @@ class _HomePageState extends State<HomePage> {
     ),
       body: Column(
         children: [
+          const SizedBox(height: 10),
+
           Expanded(child: ListView.builder(
-            padding: const EdgeInsets.all(10),
-            scrollDirection: Axis.horizontal,
-            itemCount: maps.length,
-            itemBuilder: (context, index) => MapTile(map: maps[index]),
+              padding: const EdgeInsets.all(10),
+              scrollDirection: Axis.horizontal,
+              itemCount: maps.length - 4,
+              itemBuilder: (context, index) => MapTile(map: maps[index], onTap: () => navigateToMapPage(maps[index]),),
+            ),
+          ),
 
-          )),
+          Expanded(child: ListView.builder(
+              padding: const EdgeInsets.all(10),
+              scrollDirection: Axis.horizontal,
+              itemCount: maps.length - 4 ,
+              itemBuilder: (context, index) => MapTile(map: maps[index + 4], onTap: () => navigateToMapPage(maps[index + 4]),),
+            ),
+          ),
 
-          const SizedBox(height: 200 )
 
         ],
       ),
--- strat_vault/lib/pages/intro_page.dart
+++ strat_vault/lib/pages/intro_page.dart
@@ -8,57 +8,69 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 151, 228, 151),
+      backgroundColor: const Color.fromARGB(255, 80, 88, 80),
       body: Padding(
-        padding: const EdgeInsets.all(25.0),
+        padding: const EdgeInsets.all(20.0),
+        
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(height: 25),
-              
-              // app name 
-              Text(
-                'Welcome to StratVault',
-                style: GoogleFonts.playfairDisplay (
-                  color: const Color.fromARGB(255, 54, 20, 20),
-                  fontSize: 25,
-                ),
-              ),
+              const SizedBox(height: 40),
 
+              // app name 
+              const Padding(
+                padding: EdgeInsets.only(bottom:40.0),
+                child: Text(
+                    ' StratVault',
+                    style: TextStyle (
+                      color: Color.fromARGB(255, 255, 255, 255),
+                      fontSize: 20,
+                      fontFamily: 'Penrise',
+                      letterSpacing: 1.5
+                    ),
+                  ),
+              ),
+              
+              const SizedBox(height: 10),
 
               // logo 
-              const SizedBox(height: 25),
-
               Padding(
-                padding: const EdgeInsets.all(50.0),
-                child: Image.asset('lib/images/smoke-grenade.png', height: 200, width: 200),
+                padding: const EdgeInsets.only(left:30.0, right: 50.0, top: 70.0, bottom: 10.0),
+                child: Image.asset('lib/images/logo_aplicatie_2.png', height: 300, width: 300, alignment: Alignment.center,),
+                
               ),
 
-              const SizedBox(height: 25),
-
-              Text(
+              const Text(
                 'YOUR SECRET WEAPON IN CS2',
-                style: GoogleFonts.playfairDisplay(
-                  color: const Color.fromARGB(255, 205, 152, 152),
-                  fontSize: 35,
+                style: TextStyle(
+                  color:  Color.fromARGB(255, 191, 191, 191),
+                  fontSize: 20,
+                  fontFamily: 'Tech Hard Grunge',
                 ),
               ),
 
 
-              const SizedBox(height: 10),
+              const SizedBox(height: 5),
           
 
-              Text(
-                'Explore the best CS2 lineups, tactics, and strategies for every map. From pixel-perfect smokes to deadly executes, this app gives you the winning edge in every match.',
-                style: GoogleFonts.tektur(
-                  color: const Color.fromARGB(255, 160, 100, 100),
-                  fontSize: 15,
-                  height: 1.5,
+              const Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, bottom: 5),
+                child:  Text(
+                  'Explore the best CS2 lineups, tactics, and strategies for every map. From pixel-perfect smokes to deadly executes, this app gives you the winning edge in every match.',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 187, 187, 187),
+                    fontSize: 8,
+                    height: 1.5,
+                    fontFamily: 'Camcode',
+                    
+                
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ),
 
-              const SizedBox(height: 25),
+              const SizedBox(height: 30),
 
               MyButton(text: 'Get Started', onTap: () {
                 Navigator.pushNamed(context, '/homepage');
