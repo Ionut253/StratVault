8c7712de7d2855d8d22637b2731366f0fdc694b0
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-12 19:29:14 +0200

    Implemented "Don't show again" feature

--- strat_vault/lib/pages/intro_page.dart
+++ strat_vault/lib/pages/intro_page.dart
@@ -1,83 +1,160 @@
+import 'package:animations/animations.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:strat_vault/components/button.dart';
+import 'package:strat_vault/pages/home_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
 
   @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+
+  bool _DontShowAgain = false;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkPreferences();
+  }
+
+  Future<void> _checkPreferences() async{
+      final pref = await SharedPreferences.getInstance();
+      bool shouldSkipIntro  =  pref.getBool('dontShowAgain') ?? false;
+      if (shouldSkipIntro ) {
+        Future.delayed(const Duration(milliseconds: 500), () {
+        if (mounted) {
+          Navigator.pushNamed(context, '/homepage');
+        }
+      });
+      } else {
+        setState(() {
+        _DontShowAgain = shouldSkipIntro ;
+        _isLoading = false;
+      });
+      }
+
+  }
+
+  Future<void> _savePreferences() async{
+    final pref = await SharedPreferences.getInstance();
+    await pref.setBool('dontShowAgain', _DontShowAgain);
+  }
+
+  void navigateToHomePage() {
+    Navigator.push(context, PageRouteBuilder(
+      transitionDuration: const Duration(seconds: 2),
+      pageBuilder: (context, animation, secondaryAnimation) => const HomePage(),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        return SharedAxisTransition(
+          animation: animation,
+          secondaryAnimation: secondaryAnimation,
+          transitionType: SharedAxisTransitionType.horizontal,
+          child: child,
+        );
+  }
+  ));
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      return const Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(child: CircularProgressIndicator()), // Placeholder while loading
+      );
+    }
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 80, 88, 80),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 40),
-
-              // app name 
-              const Padding(
-                padding: EdgeInsets.only(bottom:40.0),
-                child: Text(
-                    ' StratVault',
-                    style: TextStyle (
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      fontSize: 20,
-                      fontFamily: 'Penrise',
-                      letterSpacing: 1.5
-                    ),
-                  ),
-              ),
+          child: Center(
+            child: Column(
               
-              const SizedBox(height: 10),
-
-              // logo 
-              Padding(
-                padding: const EdgeInsets.only(left:30.0, right: 50.0, top: 70.0, bottom: 10.0),
-                child: Image.asset('lib/images/logo_aplicatie_2.png', height: 300, width: 300, alignment: Alignment.center,),
-                
-              ),
-
-              const Text(
-                'YOUR SECRET WEAPON IN CS2',
-                style: TextStyle(
-                  color:  Color.fromARGB(255, 191, 191, 191),
-                  fontSize: 20,
-                  fontFamily: 'Tech Hard Grunge',
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 40),
+            
+                // app name 
+                const Padding(
+                  padding: EdgeInsets.only(bottom:40.0),
+                  child: Text(
+                      ' StratVault',
+                      style: TextStyle (
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 20,
+                        fontFamily: 'Penrise',
+                        letterSpacing: 1.5
+                      ),
+                    ),
                 ),
-              ),
-
-
-              const SizedBox(height: 5),
-          
-
-              const Padding(
-                padding: EdgeInsets.only(left: 20, right: 20, bottom: 5),
-                child:  Text(
-                  'Explore the best CS2 lineups, tactics, and strategies for every map. From pixel-perfect smokes to deadly executes, this app gives you the winning edge in every match.',
+                
+                const SizedBox(height: 10),
+            
+                // logo 
+                Padding(
+                  padding: const EdgeInsets.only(left:30.0, right: 50.0, top: 70.0, bottom: 10.0),
+                  child: Image.asset('lib/images/logo_aplicatie_2.png', height: 300, width: 300, alignment: Alignment.center,),
+                  
+                ),
+            
+                const Text(
+                  'YOUR SECRET WEAPON IN CS2',
                   style: TextStyle(
-                    color: Color.fromARGB(255, 187, 187, 187),
-                    fontSize: 8,
-                    height: 1.5,
-                    fontFamily: 'Camcode',
-                    
-                
+                    color:  Color.fromARGB(255, 191, 191, 191),
+                    fontSize: 20,
+                    fontFamily: 'Tech Hard Grunge',
                   ),
-                  textAlign: TextAlign.center,
                 ),
-              ),
-
+            
+            
+                const SizedBox(height: 5),
+            
+            
+                const Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 5),
+                  child:  Text(
+                    'Explore the best CS2 lineups, tactics, and strategies for every map. From pixel-perfect smokes to deadly executes, this app gives you the winning edge in every match.',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 187, 187, 187),
+                      fontSize: 8,
+                      height: 1.5,
+                      fontFamily: 'Camcode',
+                      
+                  
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+            
               const SizedBox(height: 30),
-
+            
               MyButton(text: 'Get Started', onTap: () {
-                Navigator.pushNamed(context, '/homepage');
-              }),
-            ],
+                _savePreferences();
+                navigateToHomePage();
+                }),
+              Checkbox(
+                  value: _DontShowAgain,
+                  onChanged: (value) {
+                    setState(() {
+                      _DontShowAgain = value ?? false;
+                    });
+                  },
+                ),
+              const Text(
+                  "Don't show again",
+                  style: TextStyle(color: Colors.white),
+                ),
+                ],
+                ),
           ),
-        ),
+        )
       );
   }
 }
