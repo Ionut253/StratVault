7f97a7e3d987b856dff026dbbf6081e152e63ded
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-12 19:28:22 +0200

    Update repository.dart

--- strat_vault/lib/repository/repository.dart
+++ strat_vault/lib/repository/repository.dart
@@ -2,14 +2,14 @@ import 'package:strat_vault/models/map.dart';
 
 class Repository {
   List<CSMap> maps = [
-    CSMap(name: 'Mirage', imagePath: 'lib/images/test_map.png'),
-    CSMap(name: 'Inferno', imagePath: 'lib/images/download.jpeg'),
-    CSMap(name: 'Dust 2', imagePath: 'lib/images/test_map.png'),
-    CSMap(name: 'Nuke', imagePath: 'lib/images/test_map.png'),
-    CSMap(name: 'Overpass', imagePath: 'lib/images/test_map.png'),
-    CSMap(name: 'Anubis', imagePath: 'lib/images/test_map.png'),
-    CSMap(name: 'Ancient', imagePath: 'lib/images/test_map.png'),
-    CSMap(name: 'Train', imagePath: 'lib/images/test_map.png'),
+    CSMap(name: 'Mirage', imagePath: 'lib/images/test_map.png', calloutsPath: 'lib/images/test_map.png'),
+    CSMap(name: 'Inferno', imagePath: 'lib/images/download.jpeg', calloutsPath: 'lib/images/test_map.png'),
+    CSMap(name: 'Dust 2', imagePath: 'lib/images/test_map.png', calloutsPath: 'lib/images/test_map.png'),
+    CSMap(name: 'Nuke', imagePath: 'lib/images/test_map.png', calloutsPath: 'lib/images/test_map.png'),
+    CSMap(name: 'Overpass', imagePath: 'lib/images/test_map.png', calloutsPath: 'lib/images/test_map.png'),
+    CSMap(name: 'Anubis', imagePath: 'lib/images/anubis.jpg', calloutsPath: 'lib/images/callouts_anubis.png'),
+    CSMap(name: 'Ancient', imagePath: 'lib/images/test_map.png', calloutsPath: 'lib/images/test_map.png'),
+    CSMap(name: 'Train', imagePath: 'lib/images/test_map.png', calloutsPath: 'lib/images/test_map.png'),
   ];
 
   Repository();
