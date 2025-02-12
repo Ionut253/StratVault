d5e2319555f89a026f24ad4f4b99ee0471b5fee3
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-12 19:28:47 +0200

    Update map_page.dart

--- strat_vault/lib/pages/map_page.dart
+++ strat_vault/lib/pages/map_page.dart
@@ -1,23 +1,90 @@
-import 'package:flutter/material.dart';
 import 'package:strat_vault/models/map.dart';
+import 'package:flutter/material.dart';
 
 class MapPage extends StatelessWidget {
-
   final CSMap map;
   const MapPage({super.key, required this.map});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 151, 228, 151),
-      appBar: AppBar(
-        title: Text(map.name),
-        backgroundColor: Colors.transparent,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: const Color(0xFF576157),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          title: Text(
+            map.name,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 20,
+              fontFamily: 'Camcode',
+            ),
+          ),
+          elevation: 0,
+          leading: IconButton(
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 15,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              InteractiveViewer(
+                child: Image.asset(map.calloutsPath),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                ),
+                child: TabBar(
+                  indicatorSize:TabBarIndicatorSize.tab, 
+                  dividerColor: Colors.transparent,
+                  indicator: BoxDecoration(
+                    color: const Color.fromARGB(255, 46, 10, 10),
+                    borderRadius: BorderRadius.circular(20),
+                  ),                  
+                  tabs: const [
+                    Text(
+                        'UTILITIES',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontFamily: 'Camcode',)),
+                    Text(
+                        'CALLOUTS',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontFamily: 'Camcode',))
+                  ]
+                  ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                height: 250,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child:const TabBarView(
+                  children: [
+                    Center(child: Text('Utilities')),
+                    Center(child: Text('Callouts')),
+                  ]
+                   ),
+              )
+          ],
+          ),
+        ),
       ),
-      body: const Center(
-        child: Text('Map Page'),
-      ),
-
     );
   }
 }
