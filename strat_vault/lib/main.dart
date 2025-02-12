c8b4b6169df073103cc38c130313b078d719a2b1
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-12 19:27:34 +0200

    Update main.dart

--- strat_vault/lib/main.dart
+++ strat_vault/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:strat_vault/pages/home_page.dart';
 import 'package:strat_vault/pages/intro_page.dart';
-import 'package:strat_vault/pages/map_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'StratVault',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         useMaterial3: true,
       ),
       home: const IntroPage(),
