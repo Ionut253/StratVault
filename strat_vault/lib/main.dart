f8f326a288082adf4c66aa2451b8382b9e28b22b
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-19 19:49:09 +0200

    Update main.dart

--- strat_vault/lib/main.dart
+++ strat_vault/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:strat_vault/pages/home_page.dart';
 import 'package:strat_vault/pages/intro_page.dart';
 
@@ -11,19 +12,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'StratVault',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: const Size(360, 640),
+      child: MaterialApp(
+        title: 'StratVault',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+          useMaterial3: true,
+        ),
+        home: const IntroPage(),
+        routes: {
+          '/intropage': (context) => const IntroPage(),
+          '/homepage': (context) => const HomePage(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: const IntroPage(),
-      routes: {
-        '/intropage': (context) => const IntroPage(),
-        '/homepage': (context) => const HomePage(),
-      },
-      debugShowCheckedModeBanner: false,
-      
     );  
   }
 }
