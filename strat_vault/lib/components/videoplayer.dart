9cfe6d42f785844aa50786d8dc22deed168db1f8
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-19 19:53:38 +0200

    Create videoplayer.dart

---
+++ strat_vault/lib/components/videoplayer.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerWidget extends StatefulWidget{
+  final String videoPath;
+  const VideoPlayerWidget(this.videoPath);
+
+  @override
+  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
+}
+
+class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
+  late VideoPlayerController _controller;
+  bool _isPlaying = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget.videoPath)
+      ..initialize().then((_) {
+        setState(() {}); // Refresh UI after initialization
+        _controller.play();
+      });
+
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Dispose of the controller to prevent memory leaks
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _controller.value.isInitialized
+        ? GestureDetector(
+            onTap: () {
+              setState(() {
+                if (_controller.value.isPlaying) {
+                  _controller.pause();
+                  _isPlaying = false;
+                } else {
+                  _controller.play();
+                  _isPlaying = true;
+                }
+              });
+            },
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                AspectRatio(
+                  aspectRatio: _controller.value.aspectRatio,
+                  child: VideoPlayer(_controller),
+                ),
+                if (!_isPlaying)
+                  const Icon(
+                    Icons.play_arrow,
+                    size: 50,
+                    color: Colors.white,
+                  ),
+              ],
+            ),
+          )
+        : const Center(child: CircularProgressIndicator());
+  }
+}
