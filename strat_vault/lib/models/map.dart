a53e0cdda44bb43e637008c5dcf0dbfc352409ad
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-19 19:49:07 +0200

    Update map.dart

--- strat_vault/lib/models/map.dart
+++ strat_vault/lib/models/map.dart
@@ -2,14 +2,21 @@ class CSMap {
   String name;
   String imagePath;
   String calloutsPath;
+  Map<String, dynamic> utils = {};
+  Map<String, dynamic> strats = {};
 
   CSMap({
     required this.name, 
     required this.imagePath,
-    required this.calloutsPath
+    required this.calloutsPath,
+    required this.utils,
+    required this.strats
     });
 
   String get _name  => name;
   String get _imagePath  => imagePath;
+  String get _calloutsPath  => calloutsPath;
+  Map<String, dynamic> get _utils => utils;
+  Map<String, dynamic> get _strats => strats;
   
 }
