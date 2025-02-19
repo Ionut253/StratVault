1f5a929ea469a8b046e0a08ac4412ada94dd530b
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-19 19:48:52 +0200

    Create utility_page.dart

---
+++ strat_vault/lib/pages/utility_page.dart
@@ -0,0 +1,69 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:strat_vault/pages/map_page.dart';
+
+class UtilityPage extends StatefulWidget {
+  String utilityName;
+  List<String> utilityItems;
+
+  UtilityPage({super.key, required this.utilityName, required this.utilityItems});
+
+  @override
+  State<UtilityPage> createState() => _UtilityPageState();
+}
+
+class _UtilityPageState extends State<UtilityPage> {
+  int activeIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StatefulBuilder(
+      builder: (context, setState) {
+
+         Widget buildIndicator() => AnimatedSmoothIndicator(
+            effect: const ExpandingDotsEffect(dotWidth: 5, activeDotColor: Colors.blue),
+            activeIndex: activeIndex,
+            count: widget.utilityItems.length,
+        );
+
+      return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.grey, Colors.brown],
+          ),
+        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CarouselSlider.builder(
+                itemCount:  widget.utilityItems.length,
+                itemBuilder: (context, index, realIndex) {
+                  return InteractiveViewer(
+                    child: displayProperMedia(widget.utilityItems[index]));
+                },
+                options: CarouselOptions(
+                  enableInfiniteScroll: false,
+                  viewportFraction: 1,
+                  onPageChanged: (index, reason) => setState(() => activeIndex = index),
+                ),
+              ),
+              const SizedBox(height: 20),
+              buildIndicator(),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Close"),
+              ),
+            ],
+          ),
+      );
+    },
+  ),
+);
+  }
+}
