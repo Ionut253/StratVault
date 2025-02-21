6405f342c032f0b718c8a375f183202008036354
Ionut253 <48173899+Ionut253@users.noreply.github.com>
2025-02-21 17:54:43 +0200

    Update main.dart

--- strat_vault/lib/main.dart
+++ strat_vault/lib/main.dart
@@ -1,10 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:strat_vault/pages/home_page.dart';
 import 'package:strat_vault/pages/intro_page.dart';
+import 'package:strat_vault/repository/firestore.dart';
 
-void main() {
+void main() async{
+
+  FirestoreService firestoreService = FirestoreService();
+
+  await firestoreService.fetchImage();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(options: const FirebaseOptions(
+      apiKey: "AIzaSyALcOkHCmJgo2INXBaIa5X8_Y2evXyHZ2M",
+      authDomain: "stratvault.firebaseapp.com",
+      projectId: "stratvault",
+      storageBucket: "stratvault.firebasestorage.app",
+      messagingSenderId: "587596223692",
+      appId: "1:587596223692:web:f807cd7896819f444cde69",
+      measurementId: "G-KX05XCTCPQ"));
+  }
+  
+  await Firebase.initializeApp();
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
